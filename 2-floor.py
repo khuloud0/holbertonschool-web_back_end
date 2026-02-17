cb54935d436a68b897d3e391374ca3f8599a21aa
khuloud0 <khuloodalgarni@gmail.com>
2026-02-17 13:50:52 +0300

    Delete 2-floor.py

--- 2-floor.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Module that provides a function to return the floor of a float."""
-
-import math
-
-
-def floor(n: float) -> int:
-    """Return the floor of a float as an integer."""
-    return math.floor(n)
