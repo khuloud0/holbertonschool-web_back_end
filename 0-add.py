926b9cd9801cd916938b5acd6c0eeee952dadda4
khuloud0 <khuloodalgarni@gmail.com>
2026-02-17 13:50:31 +0300

    Delete 0-add.py

--- 0-add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Module that provides a type-annotated addition function."""
-
-
-def add(a: float, b: float) -> float:
-    """Add two float numbers and return the result as a float."""
-    return a + b
